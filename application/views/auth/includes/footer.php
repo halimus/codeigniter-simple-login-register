3b8e0804ebfa400fbf002d338ad945c51e345b99
Halim <Halim@workstation>
2016-08-04 13:29:55 -0400

    sql file edit

--- application/views/auth/includes/footer.php
+++ application/views/auth/includes/footer.php
@@ -1,2 +1,6 @@
-  </body>
+    <!--JS-->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+</body>
 </html>
