1844b963311c62558327f1ef6481650748b86bbf
LARDJANE <halim.lardjane@gmail.com>
2016-08-05 13:02:10 -0400

    forgot password

--- application/views/auth/forgot_password.php
+++ application/views/auth/forgot_password.php
@@ -3,7 +3,7 @@
     <div id="signupbox" style="margin-top:80px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
         <div class="panel panel-primary">
             <div class="panel-heading">
-                <div class="panel-title">Sign Up</div>
+                <div class="panel-title">Forgot password</div>
                 <div style="float:right; font-size: 85%; position: relative; top:-16px"><a href="<?php echo site_url('auth/login'); ?>">Sign In</a></div>
             </div>  
             <div class="panel-body" >
